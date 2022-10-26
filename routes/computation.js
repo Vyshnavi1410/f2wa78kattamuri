95cdf3cff7665c02fec3186b9c52ab0ee53f0c4e
Vyshnavi1410 <vyshnavikattamuri14@gmail.com>
2022-10-26 18:00:09 -0500

    compuattion server

--- routes/computation.js
+++ routes/computation.js
@@ -4,11 +4,13 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
     var id1 = Math.floor(Math.random()*10);
+    var id2 = Math.floor(Math.random()*10);
     var log = Math.log(id1);
-    var cosh = Math.cosh(id1);
+    var cosh = Math.cosh(id2);
     var floor = Math.floor(id1);
     res.render('computation', { title: 'Vyshnavi Kattamuri',
     id1: id1,
+    id2:id2,
     log: log,
     cosh: cosh,
     floor: floor
